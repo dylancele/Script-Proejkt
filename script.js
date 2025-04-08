9fa5734dcbdf01b8b31d3991c0d1b08fb22c1536
dylancele <dylansumner8403@gmail.com>
2025-04-08 12:28:37 +0200

    Update to traffic

--- script.js
+++ script.js
@@ -6,38 +6,72 @@ function fertig() {
     $("#LeftLane").click(LeftMove);
     $("#MiddleLane").click(MiddleMove);
     $("#RightLane").click(RightMove);
-
-    if (GameStatus == 1) {
-        
-        toggleColors(500);
+    if (GameStatus == 1) 
+    {
+        toggleColors(250);
+    }
+    if (GameStatus == 1)
+    {
+        Traffic();
+        Tree();
     }
 }
 
-function LeftMove() {
+function LeftMove() 
+{
     $("#LeftLane").append($("#ImageMain"));
 }
 
-function MiddleMove() {
+function MiddleMove() 
+{
     $("#MiddleLane").append($("#ImageMain"));
 }
 
-function RightMove() {
+function RightMove() 
+{
     $("#RightLane").append($("#ImageMain"));
 }
 
-function toggleColors(ms) {
+function toggleColors(ms) 
+{
     var isFirstColor = false;
 
     var interval = setInterval(function () {
-        if (GameStatus !== 1) {
-            clearInterval(interval);
-            return;
-        }
+    if (GameStatus !== 1) 
+    {
+    clearInterval(interval);
+    return;
+    }
 
-        $(".grass1").css("background-color", isFirstColor ? "rgba(23, 175, 9, 0.785)" : "rgba(55, 216, 41, 0.785)");
-        $(".grass2").css("background-color", isFirstColor ? "rgba(55, 216, 41, 0.785)" : "rgba(23, 175, 9, 0.785)");
-        $("#RightLane").append($("#Car1"));
+    $(".grass1").css("background-color", isFirstColor ? "rgba(23, 175, 9, 0.785)" : "rgba(55, 216, 41, 0.785)");
+    $(".grass2").css("background-color", isFirstColor ? "rgba(55, 216, 41, 0.785)" : "rgba(23, 175, 9, 0.785)");
 
-        isFirstColor = !isFirstColor;
+         isFirstColor = !isFirstColor;
     }, ms);
+    // var grass1 = $(".grass1"); 
+    // var grass2 = $(".grass2");
+    // $(grass1).animate({top:500}, 2500);
+    // $(grass2).animate({top:500}, 2500);
+}
+
+function Traffic() 
+{
+    var car = $("#drive");
+    $("#drive").css("z-index", 10);  
+    $("#drive").animate({top: 725}, 2500, function() 
+    {
+        $(car).remove();  
+    });
+}
+
+function Tree()
+{
+    var tree = $("#Tree1");  
+    var currentLane = 1;
+
+    function moveTreeDown() 
+    {
+        
+    }
+     
 }
